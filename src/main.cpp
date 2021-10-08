2aad503a89508e561b77e8f171aaf28800e66fc0
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-10-08 18:15:00 +0200

    [ARCH] Rebased into C++ Project

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-#include "gameboy/gameboy.h"
-
 int main(void)
 {
-    gameboy *new_gb = init_gameboy();
-    printf("It works !");
-    free_gameboy(new_gb);
+    return 0;
 }
